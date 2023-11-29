9ddd6733b0cedf94764a80e412c8add1bf4952be
soohwan lee <62940130+vince502@users.noreply.github.com>
2023-11-29 15:53:37 +0900

    up

--- VertexCompositeProducer/src/DStarFitter.cc
+++ VertexCompositeProducer/src/DStarFitter.cc
@@ -278,37 +278,43 @@ void DStarFitter::fitAll(const edm::Event& iEvent, const edm::EventSetup& iSetup
       TransientTrack* pionTransTkPtr = 0;
       pionTransTkPtr = &theTransTracks[trdx1];
       VertexCompositeCandidate theD0 = (*theD0Handle)[didx1];
-      auto getFreeTrajectoryStateFromFittedMother =  [&magField](const reco::VertexCompositeCandidate& fittedMother) {
-          FreeTrajectoryState trajectoryState;
+      // auto getFreeTrajectoryStateFromFittedMother =  [&magField](const reco::VertexCompositeCandidate& fittedMother) {
+      //     FreeTrajectoryState trajectoryState;
 
-          // Assuming the fittedMother has two daughters
-          const reco::Candidate* daughter1 = fittedMother.daughter(0);
-          const reco::Candidate* daughter2 = fittedMother.daughter(1);
+      //     // Assuming the fittedMother has two daughters
+      //     const reco::Candidate* daughter1 = fittedMother.daughter(0);
+      //     const reco::Candidate* daughter2 = fittedMother.daughter(1);
 
-          if (daughter1 && daughter2) {
-              // Build TransientTracks from the daughter tracks
-              reco::TransientTrack transientTrack1(*daughter2->BestTrack(), magField);
-              reco::TransientTrack transientTrack2(*daughter2->BestTrack(), magField);
+      //     if (daughter1 && daughter2) {
+      //         // Build TransientTracks from the daughter tracks
+      //         const auto& trackMass1 = fittedMother.userFloat(Form("trackMass%lu", 0));
+      //         const auto& trackMass2 = fittedMother.userFloat(Form("trackMass%lu", 1));
+      //         reco::TransientTrack transientTrack1(*daughter1->BestTrack(), magField);
+      //         reco::TransientTrack transientTrack2(*daughter2->BestTrack(), magField);
 
-              // Combine the TransientTracks to form the mother's trajectory
-              std::vector<reco::TransientTrack> tracks;
-              tracks.push_back(transientTrack1);
-              tracks.push_back(transientTrack2);
+      //         // Combine the TransientTracks to form the mother's trajectory
+      //         KinematicParticleFactoryFromTransientTrack pFactory;
+      //         float chi = 0.0;
+      //         float ndf = 0.0;
+      //         vector<RefCountedKinematicParticle> d0Particles;
+      //         d0Particles.push_back(pFactory.particle(*transientTrack1,trackMass1,chi,ndf,posCandMass_sigma[i]));
+      //         d0Particles.push_back(pFactory.particle(*transientTrack2,trackMass2,chi,ndf,negCandMass_sigma[i]));
 
-              KinematicParticleFactoryFromTransientTrack factory;
-              RefCountedKinematicTree vertexFitTree = factory.fit(tracks);
+      //         KinematicParticleVertexFitter d0Fitter;
+      //         RefCountedKinematicTree d0Vertex;
+      //         d0Vertex = d0Fitter.fit(d0Particles);
 
-              // Extract the fitted state of the mother particle
-              if (vertexFitTree->isValid()) {
-                  vertexFitTree->movePointerToTheTop();
-                  RefCountedKinematicParticle fittedParticle = vertexFitTree->currentParticle();
-                  trajectoryState = fittedParticle->currentState().freeTrajectoryState();
-              }
-          }
-          return trajectoryState;
-      };
-      const auto &theTTBuilder = iSetup.getHandle(trackBuilderToken_);
-      FreeTrajectoryState FTSD0 = getFreeTrajectoryStateFromFittedMother(theD0, theTTBuilder);
+      //         // Extract the fitted state of the mother particle
+      //         if (vertexFitTree->isValid()) {
+      //             vertexFitTree->movePointerToTheTop();
+      //             RefCountedKinematicParticle fittedParticle = vertexFitTree->currentParticle();
+      //             trajectoryState = fittedParticle->currentState().freeTrajectoryState();
+      //         }
+      //     }
+      //     return trajectoryState;
+      // };
+      GlobalTrajectoryParameters pars(GlobalPoint(theD0.vx(), theD0.vy(), theD0.vz()), GlobalVector(theD0.px(), theD0.py(), theD0.pz()), theD0.charge(), magField);
+      FreeTrajectoryState FTSD0 = FreeTrajectoryState(pars, theTTBuilder);
       
 
       // Calculate DCA of two daughters
