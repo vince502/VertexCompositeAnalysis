0053323912d37458e81164fd533b49ac1af34bc2
soohwan lee <62940130+vince502@users.noreply.github.com>
2023-12-13 12:05:20 +0900

    up

--- VertexCompositeProducer/src/DStarFitter.cc
+++ VertexCompositeProducer/src/DStarFitter.cc
@@ -281,7 +281,7 @@ void DStarFitter::fitAll(const edm::Event& iEvent, const edm::EventSetup& iSetup
       pionTransTkPtr = &theTransTracks[trdx1];
       VertexCompositeCandidate theD0 = (*theD0Handle)[didx1];
 
-      if( !pionTransTkPtr->impactPointStateAvailable()) continue;
+      // if( !pionTransTkPtr->impactPointStateAvailable()) continue;
       const auto& D0Vec = theD0.p4();
       const reco::Track& thePiTrack = pionTransTkPtr->track();
       math::PtEtaPhiMLorentzVector pPi(thePiTrack.pt(), thePiTrack.eta(), thePiTrack.phi(), piMassDStar);
