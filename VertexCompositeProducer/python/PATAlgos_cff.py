2e283b770da3186dbe564e92798c4c55945e07c2
soohwan <soohwan.lee@cern.ch>
2023-12-15 06:05:41 +0100

    update

--- VertexCompositeProducer/python/PATAlgos_cff.py
+++ VertexCompositeProducer/python/PATAlgos_cff.py
@@ -1,110 +1,150 @@
 import FWCore.ParameterSet.Config as cms
 
-def doPATMuons(process, MC=False):
+def doPATMuons(process):
     # Check if sequence already ran
-    if hasattr(process, 'patMuonsWithTrigger'): return
+    if hasattr(process, 'patMuons'): return
 
     # Make PAT Muons
-    process.load("MuonAnalysis.MuonAssociators.patMuonsWithTrigger_cff")
+    from PhysicsTools.PatAlgos.producersLayer1.muonProducer_cfi import patMuons
+    process.patMuons = patMuons.clone(
+        muonSource = cms.InputTag('muons'),
+        # track information
+        embedTrack = cms.bool(True),
+        embedCombinedMuon = cms.bool(True),
+        embedStandAloneMuon = cms.bool(True),
+        embedMuonBestTrack = cms.bool(True),
+        forceBestTrackEmbedding = cms.bool(False),
+        embedTunePMuonBestTrack = cms.bool(False),
+        embedCaloMETMuonCorrs = cms.bool(False),
+        embedTcMETMuonCorrs = cms.bool(False),
+        embedPickyMuon = cms.bool(False),
+        embedTpfmsMuon = cms.bool(False),
+        embedDytMuon = cms.bool(False),
+        # high level information
+        embedHighLevelSelection = cms.bool(True),
+        # gen information
+        addGenMatch = cms.bool(False),
+        embedGenMatch = cms.bool(False),
+        # PF information
+        useParticleFlow = cms.bool(False),
+        embedPFCandidate = cms.bool(False),
+        embedPfEcalEnergy = cms.bool(False),
+        # extra information
+        addInverseBeta = cms.bool(False),
+        addEfficiencies = cms.bool(False),
+        addResolutions = cms.bool(False),
+        userIsolation = cms.PSet(),
+        isoDeposits = cms.PSet(),
+        isolationValues = cms.PSet(),
+    )
 
-    # Add trigger matching
-    from MuonAnalysis.MuonAssociators.patMuonsWithTrigger_cff import switchOffAmbiguityResolution, addHLTL1Passthrough, useL1Stage2Candidates
-    switchOffAmbiguityResolution(process) # Switch off ambiguity resolution: allow multiple reco muons to match to the same trigger muon
-    # Use L1 Stage2 (Run2 2016-onwards)
-    addHLTL1Passthrough(process)
-    useL1Stage2Candidates(process)
-    if "hltL1extraParticles" in process.patTrigger.collections:
-        process.patTrigger.collections.remove("hltL1extraParticles")
-    if "hltGtStage2Digis:Muon" not in process.patTrigger.collections:
-        process.patTrigger.collections.append("hltGtStage2Digis:Muon")
-    process.muonMatchHLTL1.matchedCuts = cms.string('coll("hltGtStage2Digis:Muon")')
-    process.muonMatchHLTL1.useMB2InOverlap = cms.bool(True)
-    process.muonMatchHLTL1.useStage2L1 = cms.bool(True)
-    process.muonMatchHLTL1.preselection = cms.string("")
-    # Modifications for L2 and L3 HLT muons in PbPb 2018s
-    if "hltL2MuonCandidates" in process.patTrigger.collections:
-        process.patTrigger.collections.remove("hltL2MuonCandidates")
-    if "hltL2MuonCandidatesPPOnAA" not in process.patTrigger.collections:
-        process.patTrigger.collections.append("hltL2MuonCandidatesPPOnAA")
-    process.muonMatchHLTL2.matchedCuts = cms.string('coll("hltL2MuonCandidatesPPOnAA")')
-    if "hltIterL3MuonCandidates" in process.patTrigger.collections:
-        process.patTrigger.collections.remove("hltIterL3MuonCandidates")
-    if "hltIterL3MuonCandidatesPPOnAA" not in process.patTrigger.collections:
-        process.patTrigger.collections.append("hltIterL3MuonCandidatesPPOnAA")
-    process.muonMatchHLTL3.matchedCuts = cms.string('coll("hltIterL3MuonCandidatesPPOnAA")')
-    # Selection used to match online-offline muons
-    process.muonL1Info.maxDeltaR = cms.double(0.3)
-    process.muonL1Info.maxDeltaEta   = cms.double(0.2)
-    process.muonL1Info.fallbackToME1 = cms.bool(True)
-    process.muonL1Info.useStation2 = cms.bool(True)
-    process.muonL1Info.cosmicPropagationHypothesis = cms.bool(False)
-    process.muonL1Info.propagatorAlong = cms.ESInputTag("", "hltESPSteppingHelixPropagatorAlong")
-    process.muonL1Info.propagatorAny = cms.ESInputTag("", "SteppingHelixPropagatorAny")
-    process.muonL1Info.propagatorOpposite = cms.ESInputTag("", "hltESPSteppingHelixPropagatorOpposite")
-    process.muonMatchHLTL1.maxDeltaR = cms.double(0.3)
-    process.muonMatchHLTL1.maxDeltaEta   = cms.double(0.2)
-    process.muonMatchHLTL1.fallbackToME1 = cms.bool(True)
-    process.muonMatchHLTL1.useStation2 = cms.bool(True)
-    process.muonMatchHLTL1.cosmicPropagationHypothesis = cms.bool(False)
-    process.muonMatchHLTL1.propagatorAlong = cms.ESInputTag("", "hltESPSteppingHelixPropagatorAlong")
-    process.muonMatchHLTL1.propagatorAny = cms.ESInputTag("", "SteppingHelixPropagatorAny")
-    process.muonMatchHLTL1.propagatorOpposite = cms.ESInputTag("", "hltESPSteppingHelixPropagatorOpposite")
-    process.muonMatchHLTL2.maxDeltaR = cms.double(0.3)
-    process.muonMatchHLTL2.maxDPtRel = cms.double(10.0)
-    process.muonMatchHLTL3.maxDeltaR = cms.double(0.1)
-    process.muonMatchHLTL3.maxDPtRel = cms.double(10.0)
-
-    from HLTrigger.Configuration.HLT_GRun_cff import fragment
-    process.hltESPSteppingHelixPropagatorAlong = fragment.hltESPSteppingHelixPropagatorAlong
-    process.hltESPSteppingHelixPropagatorOpposite = fragment.hltESPSteppingHelixPropagatorOpposite
-
-    # Add MC gen matching
-    if MC:
-        # Prune generated particles to muons and their parents
-        process.genMuons = cms.EDProducer("GenParticlePruner",
-            src = cms.InputTag("genParticles"),
-            select = cms.vstring(
-                "drop  *  ",                      # this is the default
-                "++keep abs(pdgId) = 13"          # keep muons and their parents
-            )
-        )
-        from MuonAnalysis.MuonAssociators.patMuonsWithTrigger_cff import addMCinfo
-        addMCinfo(process)
-        # since we match inner tracks, keep the matching tight and make it one-to-one
-        process.muonMatch.maxDeltaR = cms.double(0.05)
-        process.muonMatch.resolveByMatchQuality = cms.bool(True)
-        process.muonMatch.matched = cms.InputTag("genMuons")
+    process.patMuons.userData.userInts.src    = []
+    process.patMuons.userData.userFloats.src  = []
+    process.patMuons.userData.userCands.src   = []
+    process.patMuons.userData.userClasses.src = []
 
     # Make a sequence
-    if MC:
-        process.patMuonSequence = cms.Sequence( process.genMuons + process.patMuonsWithTriggerSequence )
-    else:
-        process.patMuonSequence = cms.Sequence( process.patMuonsWithTriggerSequence )
+    process.patMuonSequence = cms.Sequence( process.patMuons )
+
+
+def doPATElectrons(process):
+    # Check if sequence already ran
+    if hasattr(process, 'patElectrons'): return
+
+    # Make PAT Electrons
+    from PhysicsTools.PatAlgos.producersLayer1.electronProducer_cfi import patElectrons
+    process.patElectrons = patElectrons.clone(
+        electronSource = cms.InputTag('gedGsfElectrons'),
+        # track information
+        embedTrack                  = cms.bool(True),
+        embedGsfElectronCore        = cms.bool(True),
+        embedGsfTrack               = cms.bool(True),
+        embedSuperCluster           = cms.bool(True),
+        embedSeedCluster            = cms.bool(True),
+        embedBasicClusters          = cms.bool(True),
+        embedPreshowerClusters      = cms.bool(False),
+        embedRecHits                = cms.bool(False),
+        # high level information
+        embedHighLevelSelection = cms.bool(True),
+        # gen information
+        addGenMatch = cms.bool(False),
+        embedGenMatch = cms.bool(False),
+        # PF information
+        embedPflowSuperCluster      = cms.bool(False),
+        embedPflowBasicClusters     = cms.bool(False),
+        embedPflowPreshowerClusters = cms.bool(False),
+        embedPFCandidate            = cms.bool(False),
+        # extra information
+        addMVAVariables         = cms.bool(False),
+        isoDeposits             = cms.PSet(),
+        isolationValues         = cms.PSet(),
+        isolationValuesNoPFId   = cms.PSet(),
+    )
+
+    process.patElectrons.userData.userInts.src    = []
+    process.patElectrons.userData.userFloats.src  = []
+    process.patElectrons.userData.userCands.src   = []
+    process.patElectrons.userData.userClasses.src = []
+
+    process.patLowPtElectrons = process.patElectrons.clone(electronSource = cms.InputTag('lowPtGsfElectrons'))
+
+    # Make a sequence
+    process.patElectronSequence = cms.Sequence( process.patElectrons * process.patLowPtElectrons )
+
+
+def doPATPhotons(process):
+    # Check if sequence already ran
+    if hasattr(process, 'patPhotons'): return
+
+    # Make PAT Electrons
+    from PhysicsTools.PatAlgos.producersLayer1.photonProducer_cfi import patPhotons
+    process.patPhotons = patPhotons.clone(
+        photonSource = cms.InputTag('gedPhotons'),
+        electronSource = cms.InputTag('gedGsfElectrons'),
+        # photon information
+        embedSuperCluster           = cms.bool(True),
+        embedSeedCluster            = cms.bool(True),
+        embedBasicClusters          = cms.bool(True),
+        embedPreshowerClusters      = cms.bool(True),
+        embedRecHits                = cms.bool(True),
+        saveRegressionData          = cms.bool(True),
+        # high level information
+        addPhotonID                 = cms.bool(False),
+        # gen information
+        addGenMatch = cms.bool(False),
+        embedGenMatch = cms.bool(False),
+        # PF information
+        addPFClusterIso             = cms.bool(False),
+        addPuppiIsolation           = cms.bool(False),
+        # extra information
+        isoDeposits             = cms.PSet(),
+        userIsolation           = dict()
+    )
+
+    process.patPhotons.userData.userInts.src    = []
+    process.patPhotons.userData.userFloats.src  = []
+    process.patPhotons.userData.userCands.src   = []
+    process.patPhotons.userData.userClasses.src = []
+
+    # Make a sequence
+    process.patPhotonSequence = cms.Sequence( process.patPhotons )
 
 
 def changeToMiniAOD(process):
 
-    if hasattr(process, "patMuonsWithTrigger"):
-        from MuonAnalysis.MuonAssociators.patMuonsWithTrigger_cff import useExistingPATMuons
-        useExistingPATMuons(process, newPatMuonTag=cms.InputTag("unpackedMuons"), addL1Info=False)
+    process.patTriggerFull = cms.EDProducer("PATTriggerObjectStandAloneUnpacker",
+        patTriggerObjectsStandAlone = cms.InputTag('slimmedPatTrigger'),
+        triggerResults              = cms.InputTag('TriggerResults::HLT'),
+        unpackFilterLabels          = cms.bool(True)
+    )
+    process.load('VertexCompositeAnalysis.VertexCompositeProducer.unpackedTracksAndVertices_cfi')
+    process.eventFilter_HM.insert(0, process.unpackedTracksAndVertices)
 
-        process.patTriggerFull = cms.EDProducer("PATTriggerObjectStandAloneUnpacker",
-            patTriggerObjectsStandAlone = cms.InputTag('slimmedPatTrigger'),
-            triggerResults              = cms.InputTag('TriggerResults::HLT'),
-            unpackFilterLabels          = cms.bool(True)
-        )
-        process.load('PhysicsTools.PatAlgos.slimming.unpackedTracksAndVertices_cfi')
-        process.eventFilter_HM.insert(0, process.unpackedTracksAndVertices)
+    if hasattr(process, "patMuons"):
         process.load('VertexCompositeAnalysis.VertexCompositeProducer.unpackedMuons_cfi')
-        process.eventFilter_HM.insert(1, process.unpackedMuons)
+        process.patMuons = process.unpackedMuons.clone()
 
-    if hasattr(process, "output_HM"):
-        process.output_HM.outputCommands.append('keep *Vert*_unpackedTracksAndVertices_*_*')
-        process.output_HM.outputCommands.append('keep patMuons_unpackedMuons_*_*')
-        process.output_HM.outputCommands.append('drop patMuons_patMuonsWith*_*_*')
-
-
-    from HLTrigger.Configuration.CustomConfigs import MassReplaceInputTag
+    from Configuration.Applications.ConfigBuilder import MassReplaceInputTag
     process = MassReplaceInputTag(process,"offlinePrimaryVertices","unpackedTracksAndVertices")
     process = MassReplaceInputTag(process,"generalTracks","unpackedTracksAndVertices")
-    process = MassReplaceInputTag(process,"muons","unpackedMuons")
+    process = MassReplaceInputTag(process,"genParticles","prunedGenParticles")
