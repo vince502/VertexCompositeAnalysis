1ce430b496763856bf7fd49755a9e2affdbc8cb2
JunseokLee3609 <jun502s@korea.ac.kr>
2025-07-01 10:48:17 +0200

    up

--- VertexCompositeAnalyzer/plugins/PATCompositeTreeProducer2.cc
+++ VertexCompositeAnalyzer/plugins/PATCompositeTreeProducer2.cc
@@ -358,10 +358,6 @@ PATCompositeTreeProducer2::fillRECO(const edm::Event& iEvent, const edm::EventSe
 
 
              //if(debug_ ) std::cout << "nGenDau: " << nGenDau<< std::endl;
-              #ifdef DEBUG
-              cout << "matchGEN[it]: " << matchGEN[it] << endl;
-              cout << "nGen : " << nGen << endl;
-                #endif
 
               matchGEN[it] = matchGEN[it] || (matchHadron(recoD1, *theGenD0,true) && matchHadron(recoPi, *theGenPion,false));
               #ifdef DEBUG
@@ -383,26 +379,27 @@ PATCompositeTreeProducer2::fillRECO(const edm::Event& iEvent, const edm::EventSe
                 #endif
                 if(matchGEN[it]){
 
-                  isSwap[it] = checkSwap(recoD1, *theGenD0);
                 #ifdef DEBUG
-                  if(isSwap[it]){
                 cout << "genRefs.at(igen) : " << genRefs.at(igen)->pdgId() << endl;
-                cout << "nGenDau : " << nGenDau << endl;
-                cout << "Gen D0_dau1 : " << theGenD0->daughter(0)->pdgId() << endl;
-                cout << "Gen D0_dau2 : " << theGenD0->daughter(1)->pdgId() << endl;
+                cout << "DStar pdgID : " << theGenDStar->pdgId() << endl;
+                cout << "D0_dau1 : " << theGenD0->daughter(0)->pdgId() << endl;
+                cout << "D0_dau2 : " << theGenD0->daughter(1)->pdgId() << endl;
                 cout << "GenDstar_dau1 : " << theGenD0->pdgId() << endl;
                 cout << "GenDstar_dau2 : " << theGenPion->pdgId() << endl;
                 cout << "RecoDstar_dau1 : " << recoD1->pdgId() << endl;
                 cout << "RecoDstar_dau2 : " << recoPi->pdgId() << endl;
                 cout << "match D0 : " << matchHadron(recoD1, *theGenD0,true) << endl;
                 cout << "match Pion : " << matchHadron(recoPi, *theGenPion,false) << endl;
-                  }
                 #endif
+                  isSwap[it] = checkSwap(recoD1, *theGenD0);
                   auto mom_ref = findMother(theGenDStar);
                   if (mom_ref.isNonnull()) idmom_reco[it] = mom_ref->pdgId();
                   int __count_anc__ = 0;
                   auto __ref_anc__ = mom_ref;
                   while ( __ref_anc__.isNonnull() && __count_anc__ < 50 ){
+                  #ifdef DEBUG
+                    cout << "ref ancestor: " <<  __ref_anc__->pdgId() << endl;
+                  #endif
                     __ref_anc__ = findMother(__ref_anc__);
                     if( __ref_anc__.isNonnull()){
                       if( ((int) abs(__ref_anc__->pdgId())) % 1000 / 100 == 5){ 
@@ -424,8 +421,8 @@ PATCompositeTreeProducer2::fillRECO(const edm::Event& iEvent, const edm::EventSe
                   matchGen_D0charge_[it] = theGenD0->charge();
                   matchGen_D0pdgId_[it] = theGenD0->pdgId();
 
-                  genDecayLength(*theGenD0, matchGen_D1decayLength2D_[it], matchGen_D1decayLength3D_[it], matchGen_D1angle2D_[it], matchGen_D1angle3D_[it] );
-                  getAncestorId(*theGenD0, matchGen_D1ancestorId_[it], matchGen_D1ancestorFlavor_[it] );
+                  genDecayLength(*theGenDStar, matchGen_D1decayLength2D_[it], matchGen_D1decayLength3D_[it], matchGen_D1angle2D_[it], matchGen_D1angle3D_[it] );
+                  getAncestorId(*theGenDStar, matchGen_D1ancestorId_[it], matchGen_D1ancestorFlavor_[it] );
 
                   const auto* genDau0 = theGenD0->daughter(0);
                   const auto* genDau1 = theGenD0->daughter(1);
@@ -1058,7 +1055,6 @@ PATCompositeTreeProducer2::fillRECO(const edm::Event& iEvent, const edm::EventSe
                   grand_T4dedx1[it] = dEdxTrack[gdau1].dEdx();
                   grand_T4dedx2[it] = dEdxTrack[gdau2].dEdx();
               }
-    
               
               //track pt
               grand_pt1[it] = gd1->pt();
@@ -1067,7 +1063,6 @@ PATCompositeTreeProducer2::fillRECO(const edm::Event& iEvent, const edm::EventSe
               //track momentum
               grand_p1[it] = gd1->p();
               grand_p2[it] = gd2->p();
-
               
               //track eta
               grand_eta1[it] = gd1->eta();
@@ -1076,22 +1071,6 @@ PATCompositeTreeProducer2::fillRECO(const edm::Event& iEvent, const edm::EventSe
               //track charge
               grand_charge1[it] = gd1->charge();
               grand_charge2[it] = gd2->charge();
-#ifdef DEBUG
-          math::XYZTLorentzVector dau1_p4(gd1->px(), gd1->py(), gd1->pz(), gd1->energy());
-          math::XYZTLorentzVector dau2_p4(gd2->px(), gd2->py(), gd2->pz(), gd2->energy());
-          math::XYZTLorentzVector D0_daughters_p4 = dau1_p4 + dau2_p4;
-          if(fabs(d1->pt()-D0_daughters_p4.pt()) > 0.001){
-          std::cout << "Combined D0 daughters pT: " << D0_daughters_p4.pt() << std::endl;
-          std::cout << "Combined D0 daughters eta: " << D0_daughters_p4.eta() << std::endl;
-          std::cout << "Combined D0 daughters phi: " << D0_daughters_p4.phi() << std::endl;
-          std::cout << "Combined D0 daughters mass: " << D0_daughters_p4.mass() << std::endl;
-          std::cout << "pdgID dau1: " << d1->daughter(0)->pdgId() << std::endl;
-          std::cout << "pdgID dau2: " << d1->daughter(1)->pdgId() << std::endl;
-          std::cout << "D0 pdg Id " << d1->pdgId() << std::endl;
-          std::cout << "D0 # of daughters: " << d1->numberOfDaughters() << std::endl;
-          std::cout << d1->pt()-D0_daughters_p4.pt() << std::endl;
-          }
-#endif
               
               //track Chi2
               grand_trkChi1[it] = gdau1->normalizedChi2();
@@ -1287,11 +1266,9 @@ PATCompositeTreeProducer2::fillRECO(const edm::Event& iEvent, const edm::EventSe
             do {
               auto Dd1 = trk.daughter( permutations.at(0) );
               auto Dd2 = trk.daughter( permutations.at(1) );
-             
-  
               if (abs(Dd1->pdgId()) == PID_dau1_ && abs(Dd2->pdgId()) == PID_dau2_) {
-                if (Dd1->numberOfDaughters() != 2) continue; 
                 if(twoLayerDecay_){
+                  if (Dd1->numberOfDaughters() != 2) continue;
                   // Magic numbers, _permutations -> number of D0 daughters;
                   std::vector<unsigned int> _permutations(2);
                   std::iota(_permutations.begin(), _permutations.end(), 0);
@@ -1342,13 +1319,10 @@ PATCompositeTreeProducer2::fillRECO(const edm::Event& iEvent, const edm::EventSe
           #endif
           auto const theGenDStar = genRefs.at(igen);
           if(abs(theGenDStar->pdgId())!=413) cout << "id : " << theGenDStar->pdgId() << endl;
-          unsigned int idxD0 = 1;
-          if(fabs(theGenDStar->daughter(0)->pdgId())==211)  std::cout << theGenDStar->daughter(0)->pdgId() << std::endl;
+          unsigned int idxD0 = -1;
           if( fabs(theGenDStar->daughter(0)->pdgId()) == 421 ) idxD0 = 0;
           auto const* theGenD0 = genRefs.at(igen)->daughter(idxD0);
           auto const* theGenPion = genRefs.at(igen)->daughter(1- idxD0);
-          if (theGenD0->numberOfDaughters() != 2)
-                  std::cout << "asdfasdf" << std::endl;
           mass_gen[igen] = theGenDStar->mass();
           pt_gen[igen] = theGenDStar->pt();
           eta_gen[igen] = theGenDStar->eta(); 
@@ -1378,7 +1352,6 @@ PATCompositeTreeProducer2::fillRECO(const edm::Event& iEvent, const edm::EventSe
           gen_D1y_[igen] = theGenPion->rapidity();
           gen_D1pdgId_[igen] = theGenPion->pdgId();
 
-
           const auto* genDau0 = theGenD0->daughter(0);
           const auto* genDau1 = theGenD0->daughter(1);
           gen_D0Dau1_pT_[igen] = genDau0->pt();
@@ -1386,40 +1359,18 @@ PATCompositeTreeProducer2::fillRECO(const edm::Event& iEvent, const edm::EventSe
           gen_D0Dau1_phi_[igen] = genDau0->phi();
           gen_D0Dau1_y_[igen] = genDau0->rapidity();
           gen_D0Dau1_pdgId_[igen] = genDau0->pdgId();
-          gen_D0Dau1_mass_[igen] = genDau0->mass();
           #ifdef DEBUG
           //cout << "D0 dau1 pdgId : " << genDau0->pdgId() << endl;
-	      cout <<"D0 Ancestor Id : " <<  gen_D0ancestorId_[igen] << endl;
+	     //cout <<"D0 Ancestor Id : " <<  gen_D0ancestorId_[igen] << endl;
           #endif
-          math::XYZTLorentzVector dau1_p4(genDau0->px(), genDau0->py(), genDau0->pz(), genDau0->energy());
-          math::XYZTLorentzVector dau2_p4(genDau1->px(), genDau1->py(), genDau1->pz(), genDau1->energy());
-
-
-#ifdef DEBUG
-          math::XYZTLorentzVector D0_daughters_p4 = dau1_p4 + dau2_p4;
-          if(fabs(theGenD0->pt()-D0_daughters_p4.pt()) > 0.001){
-          std::cout << "Combined D0 daughters pT: " << D0_daughters_p4.pt() << std::endl;
-          std::cout << "Combined D0 daughters eta: " << D0_daughters_p4.eta() << std::endl;
-          std::cout << "Combined D0 daughters phi: " << D0_daughters_p4.phi() << std::endl;
-          std::cout << "Combined D0 daughters mass: " << D0_daughters_p4.mass() << std::endl;
-          std::cout << "pdgID dau1: " << theGenD0->daughter(0)->pdgId() << std::endl;
-          std::cout << "pdgID dau2: " << theGenD0->daughter(1)->pdgId() << std::endl;
-          std::cout << "D0 pdg Id " << theGenD0->pdgId() << std::endl;
-          std::cout << "D0 # of daughters: " << theGenD0->numberOfDaughters() << std::endl;
-          std::cout << theGenD0->pt()-D0_daughters_p4.pt() << std::endl;
-          }
-#endif
           // cout << "D0 dau1 pdgId : " <<  gen_D0Dau1_pdgId_[igen] << endl;
           gen_D0Dau2_pT_[igen] = genDau1->pt();
+
           gen_D0Dau2_eta_[igen] = genDau1->eta();
           gen_D0Dau2_phi_[igen] = genDau1->phi();
           gen_D0Dau2_y_[igen] = genDau1->rapidity();
           gen_D0Dau2_pdgId_[igen] = genDau1->pdgId();
-          gen_D0Dau2_mass_[igen] = genDau1->mass();
-
-
-          
-        }
+      }
     }
     else{
         for( unsigned int igen=0; igen<nGen; igen++){
--- VertexCompositeProducer/src/DStarFitter.cc
+++ VertexCompositeProducer/src/DStarFitter.cc
@@ -391,27 +391,7 @@ void DStarFitter::fitAll(const edm::Event& iEvent, const edm::EventSetup& iSetup
       //  cout << "slowPion PdgId : " << pionTrackRef->pdgId() << " kaonCand PdgId : " << kaonCand->pdgId() << " pionCand PdgId : " << pionCand->pdgId() << endl;
        #endif
 
-       int slowPionCharge = pionTrackRef->charge();
-       
-       reco::Candidate* kaonCand = nullptr;
-       reco::Candidate* pionCand = nullptr;
-       
-       
-       if (dau0->mass() > dau1->mass()) {
-         kaonCand = dau0;
-         pionCand = dau1;
-       } else {
-         kaonCand = dau1;
-         pionCand = dau0;
-       }
-       
-       // For D*+: K- pi+ followed by slow pi+
-       // For D*-: K+ pi- followed by slow pi-
-       if (slowPionCharge > 0) { 
-         if (kaonCand->charge() > 0) continue;
-       } else { 
-         if (kaonCand->charge() < 0) continue;
-       }
+
 
        reco::TransientTrack ttk0(*dau0->bestTrack(), magField);
        reco::TransientTrack ttk1(*dau1->bestTrack(), magField);
